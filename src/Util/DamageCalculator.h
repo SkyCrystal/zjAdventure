5c7bd32a0f204c4f6520b48fbdee67eb5773f505
pb <pb2001@163.com>
2025-05-17 15:54:49 +0800

    优化伤害计算器代码

--- src/Util/DamageCalculator.h
+++ src/Util/DamageCalculator.h
@@ -7,26 +7,17 @@
 
 enum class DamageCalcType {
   FIXED,
-  PERCENTAGE,
+  ATK_PERCENTAGE,
 };
 template <DamageCalcType dmg_type>
 class DamageCalculator {
  public:
-  static int calculate(std::weak_ptr<ICharacter> from) {
+  static int calculate(std::weak_ptr<ICharacter> from, int sum) {
     switch (dmg_type) {
       case DamageCalcType::FIXED:
-        assert(false);
-        return 0;
-      case DamageCalcType::PERCENTAGE:
-        return from.lock()->getAttackPower();
-      default:
-        assert(false);
-    }
-  }
-  static int calculate(int dmg) {
-    switch (dmg_type) {
-      case DamageCalcType::FIXED:
-        return dmg;
+        return sum;
+      case DamageCalcType::ATK_PERCENTAGE:
+        return from.lock()->getAttackPower() * sum / 100;
       default:
         assert(false);
         return 0;
