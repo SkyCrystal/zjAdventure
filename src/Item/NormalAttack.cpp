76c365e05331f8814631f9a4d3fd224d34f5c864
pb <pb2001@163.com>
2025-05-17 16:47:33 +0800

    普通攻击

--- src/Item/NormalAttack.cpp
+++ src/Item/NormalAttack.cpp
@@ -1,6 +1,13 @@
 #include "NormalAttack.h"
+#include "Action/Damage/NormalDamage.h"
+#include "IAction.h"
 
 std::vector<std::shared_ptr<IAction>> NormalAttack::onAction(
     std::shared_ptr<IAction> action) {
-  return {action};
+  switch (action->GetType()) {
+    case ActionType::ROUND_START:
+      return {std::make_shared<NormalDamage>(getOwner())};
+    default:
+      return {};
+  }
 }
